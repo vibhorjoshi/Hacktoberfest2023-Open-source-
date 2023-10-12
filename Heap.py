5ced84e5d6aced12667c8b783ba401dcae18edc0
vibhorjoshi <105739194+vibhorjoshi@users.noreply.github.com>
2023-10-12 13:27:48 +0700

    Create Heap.py

---
+++ Heap.py
@@ -0,0 +1,6 @@
+Operations of Heap Data Structure:
+
+Heapify: a process of creating a heap from an array.
+Insertion: process to insert an element in existing heap time complexity O(log N).
+Deletion: deleting the top element of the heap or the highest priority element, and then organizing the heap and returning the element with time complexity O(log N).
+Peek: to check or find the first (or can say the top) element of the heap.
